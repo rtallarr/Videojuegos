c8cd169c72a1384aa13c4d7dfd0a2efd0ce3d1d8
JOAQUIN DE FERRARI GONZALEZ <joaquin.deferrari@sansano.usm.cl>
2023-05-11 17:11:53 -0400

    Proyecto Base

--- Scripts/MovimientoPJ.cs
+++ Scripts/MovimientoPJ.cs
@@ -10,7 +10,6 @@ public class MovimientoPJ : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Probando GITHUB")
     }
 
     // Update is called once per frame
