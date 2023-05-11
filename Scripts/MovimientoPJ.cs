9c4c68ef70192bb8f2ef77380c907ede9d11df41
JOAQUIN DE FERRARI GONZALEZ <joaquin.deferrari@sansano.usm.cl>
2023-05-11 16:49:49 -0400

    Probando git por primera vez

--- Scripts/MovimientoPJ.cs
+++ Scripts/MovimientoPJ.cs
@@ -10,7 +10,7 @@ public class MovimientoPJ : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Probando GITHUB")
     }
 
     // Update is called once per frame
