800540cea7e316a0d377284149087b2540e5261e
Joacodef <joaquin.deferrari@sansano.usm.cl>
2023-05-15 15:57:11 -0400

    Agregadas escenas de menu principal y game over, con botones que inician o hacen exit. Agregadas paredes que deberían matar al jugador.

--- Scripts/Colisiones.cs
+++ Scripts/Colisiones.cs
@@ -12,24 +12,11 @@ public class Colisiones : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision)
-
-
     {
-        Debug.Log("Do something here");
-
-        Destroy(collision.gameObject);
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (collision.gameObject.name == "MyGameObjectName")
-        {
-            //If the GameObject's name matches the one you suggest, output this message in the console
-            Debug.Log("Do something here");
-        }
-
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "MyGameObjectTag")
+        if (collision.gameObject.tag == "Proyectil")
         {
-            //If the GameObject has the same tag as specified, output this message in the console
-            Debug.Log("Do something else here");
+            Destroy(collision.gameObject);
         }
     }
 }
