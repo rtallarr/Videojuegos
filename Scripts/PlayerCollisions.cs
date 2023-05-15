e8643b16d8d342a86d26ff5d6eaa4bbe3d194694
Joacodef <joaquin.deferrari@sansano.usm.cl>
2023-05-15 17:06:24 -0400

    Agregada barra de vida. La barra verde se desplaza un poco hacia la izquieda respecto a la barra roja.

--- Scripts/PlayerCollisions.cs
+++ Scripts/PlayerCollisions.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Colisiones : MonoBehaviour
+public class PlayerCollisions : MonoBehaviour
 {
+    
     
     // Update is called once per frame
     void Update()
@@ -13,10 +15,18 @@ public class Colisiones : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collision detected");
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (collision.gameObject.tag == "Proyectil")
         {
             Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Muralla")
+        {
+            // Change scene to the game over scene
+            Debug.Log("Game Over");
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
